80fa1254af7a78c16fb0642877857d2e745d56da
kathleendiergaardt5-sys <kathleendiergaardt5@gmail.com>
2026-08-10 03:30:10 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { tanstackStart } from '@tanstack/react-start/config'
 
 export default defineConfig({
   plugins: [
-    tanstackStart(),
     tsconfigPaths(),
     react(),
   ],
