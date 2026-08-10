c6d68e146b403b9e970dd25776cf16888cd2eac2
kathleendiergaardt5-sys <kathleendiergaardt5@gmail.com>
2026-08-10 02:57:21 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { TanStackStartVite } from '@tanstack/react-start/plugin'
+import { tanstackStart } from '@tanstack/react-start/config'
 
 export default defineConfig({
   plugins: [
-    TanStackStartVite({
-      deployment: {
-        target: 'vercel',
-      },
-    }),
+    tanstackStart(),
     tsconfigPaths(),
     react(),
   ],
